f0eb32387de272693177b8fef11f7bd26a78218d
Okadarien Saru <okadas@tanban.org>
2021-01-24 01:51:38 +0900

    travis config and test script modified for master branch (including docker login/push), test script fixed for test branch, version file removed from Debian-* directories

--- travis_docker_build.sh
+++ travis_docker_build.sh
@@ -3,12 +3,27 @@
 app_branch="${TRAVIS_BRANCH%%-dev}"
 : ${app_branch:?app_branch not set!};
 
-if [ "${TRAVIS_BRANCH}" = test ]; then
+case "${TRAVIS_BRANCH}" in
+  master) 
+  docker login \
+    -u "${DOCKERHUB_USER:?DOCKERHUB_USER not set} \
+    -p "${DOCKERHUB_SECRET:?DOCKERHUB_SECRET not set} \
+    || { echo Cannot login to DockerHub!; exit 1;}
   for i in */*; do
+    app=${i%%_*}:${i#*_}
+    app=$(echo ${app:?app not set} | tr A-Z a-z)
+    docker build -t "coshapp/$app" -f $i .
+    docker push "coshapp/$app"
+  done ;;
+
+  test)
+  for i in */*; do
+    app=${i%%_*}:${i#*_}
+    app=$(echo ${app:?app not set} | tr A-Z a-z)
     docker build -t "localhost/$app" -f $i .
     docker rmi localhost/$app
-  done
-else
+  done ;;
+  *-dev)
   for i in $(ls */*); do
     app=$(echo $i | tr A-Z a-z)
     app=${app##*/}
@@ -25,5 +40,6 @@ else
     echo "$app" | grep -iE "^${app_branch:?app_branch not set!}-l10n-.*" \
       && docker build -t "localhost/$app" -f $i . \
       || echo $app skipped.
-  done
-fi
+  done ;;
+esac
+
