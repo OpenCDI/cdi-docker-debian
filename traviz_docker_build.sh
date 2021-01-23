bf70662a3bad41bd0fc06db2726560c566f3e21b
Okadarien Saru <okadas@tanban.org>
2021-01-23 23:33:44 +0900

    traviz test script added

---
+++ traviz_docker_build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+app_branch="${TRAVIZ_BRANCH##dev-app-}"
+: ${app_branch:?app_branch not set!};
+
+if [ "${TRAVIZ_BRANCH}" = test ]; then
+  for i in */*; do
+    docker build -t "localhost/$app" -f $i .
+    docker rmi localhost/$app
+  done
+else
+  for i in $(ls */*) do
+    app=$(echo $i | tr A-Z a-z)
+    : ${app:?app name not set for $i!}
+    [ "$app" = "${app_branch:?app_branch not set!}" ] \
+      && {
+        docker rmi $(docker images | grep localhost\/)
+        docker build -t "localhost/$app" -f $i .
+      }
+    echo "$app" | grep -iE "^${app_branch:?app_branch not set!}-l10n-.*" \
+      && docker build -t "localhost/$app" -f $i .
+  done
+fi
