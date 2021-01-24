f3c9a6cd83147f9c83981681fc371940ed040a68
Okadarien Saru <okadas@tanban.org>
2021-01-24 16:52:12 +0900

    Travis build script was separated in two pieces, placed in .travis_scripts. Fixed .travis.yml (with secrets).

--- .travis_scripts/travis_docker_build.sh
+++ .travis_scripts/travis_docker_build.sh
@@ -17,11 +17,8 @@ case "${TRAVIS_BRANCH}" in
     app=$(echo ${app:?app not set} | tr A-Z a-z)
     app=${app#*/}
     docker build -t "coshapp/$app" -f $i .
-    docker push "coshapp/$app"
     docker tag "coshapp/$app" "coshapp/${app}-${COSHAPP_BASE_VERSION:-10.7}"
-    docker push "coshapp/${app}-${COSHAPP_BASE_VERSION:-10.7}"
     [ "$app" = "debian" ] && docker tag "coshapp/$app" "coshapp/${app%%:*}:latest"
-    docker push "coshapp/${app%%:*}:latest"
   done ;;
 
   test)
---
+++ .travis_scripts/travis_docker_push.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -ex
+
+app_branch=$(echo $TRAVIS_BRANCH | tr A-Z a-z)
+app_branch="${app_branch%%-dev}"
+: ${app_branch:?app_branch not set!};
+
+case "${TRAVIS_BRANCH}" in
+  master) 
+  echo "${DOCKERHUB_SECRET:?DOCKERHUB_SECRET not set} \
+    | docker login \
+      --username "${DOCKERHUB_USER:?DOCKERHUB_USER not set} \
+      --password-stdin \
+      || { echo Cannot login to DockerHub!; exit 1;}
+  for i in */*; do
+    app=${i%%_*}:${i#*_}
+    app=$(echo ${app:?app not set} | tr A-Z a-z)
+    app=${app#*/}
+    docker push "coshapp/$app"
+    docker push "coshapp/${app}-${COSHAPP_BASE_VERSION:-10.7}"
+    [ "$app" = "debian" ] && docker push "coshapp/${app%%:*}:latest"
+  done ;;
+
+  *) return 0 ;;
+esac
+
