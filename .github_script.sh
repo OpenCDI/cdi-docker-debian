bb12c53d43595eb812e6e6962c77ec9089cf3803
Okadarien Saru <okadas@tanban.org>
2021-02-03 08:08:31 +0900

    [bugfix] make ci helper script not to stop at any functions return 1

--- .github_script.sh
+++ .github_script.sh
@@ -6,7 +6,7 @@ set_image_name(){
   image_name="$(echo $1 | tr A-Z a-z | sed s/_\.\*//g)"
   image_name="${image_name#*/}"
   tag_name="$(echo $1 | tr A-Z a-z | sed s/\.\*_//g)"
-  test ! -z "$tag_name" || tag_name=null
+  [ ! -z "$tag_name" ] || tag_name=null
   img_desc="${LOGIN_NAME:-coshapp}/${image_name:?no imagename}:${tag_name:?no tagname}-${coshapp_ver:?no ver}${TAG_POSTFIX:+-$TAG_POSTFIX}" 
 }
 
@@ -28,7 +28,7 @@ build_image(){
       : ${tag_name:? tag_name not set} 
       is_non_l10n_img && is_non_test_img && {
           docker tag $img_desc ${img_desc%%:*}:latest
-      }
+      } || echo skipping...
     done
   done
 }
@@ -43,7 +43,7 @@ push_image(){
       docker push $img_desc
       is_non_l10n_img && is_non_test_img && {
           docker push ${img_desc%%:*}:latest
-      }
+      } || echo skipping...
     done
   done
 }
